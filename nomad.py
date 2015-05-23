437ff9a613be5148b2508726514eb8ab1557fdc9
He Li <business.he.li@gmail.com>
2015-05-22 21:24:34 -0500

    first draft done

--- nomad.py
+++ nomad.py
@@ -1,7 +1,6 @@
 import requests
 import json
 
-
 url = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyAZV-zJJ1GG8Mglx7y64wQ9_Kfc0sKHapE'
 
 airports = [raw_input('Enter the starting airport: ')]
@@ -10,9 +9,9 @@ dates = [raw_input('Enter the starting date (YYYY-MM-DD): ')]
  
 while True:
   new_ap = raw_input('Enter a new destination airport: ')
+  airports.append(new_ap)
   final = raw_input('Is this your final destination [y/n]? ').lower()[0] 
   if final == 'n':
-    airports.append(new_ap)
     #durations.append(int(raw_input('Enter how many days you will stay there \
     #                                (0 if leaving the same day): ')))
     dates.append(raw_input('Enter the day you leave this destination (YYYY-MM-DD): '))
@@ -22,34 +21,27 @@ while True:
 #airports = ['BOS', 'ORD', 'SFO'] #, 'MSP', 'TUS']
 results = {}
 
-for i in xrange(len(airports)):
+for i in xrange(len(airports)-1):
   origin = airports[i]
-  results[origin] = {}
-  dests = airports[:i] + airports[i+1:]
-  for dest in dests:
-    data = \
-      { 
-        "request": {
-          "passengers": {
-            "adultCount": 3
+  dest = airports[i+1]
+  data = \
+    { 
+      "request": {
+        "passengers": {
+          "adultCount": 1
+        },
+        "slice": [
+          {
+            "origin": origin,
+            "destination": dest,
+            "date": dates[i]
           },
-          "slice": [
-            {
-              "origin": origin,
-              "destination": dest,
-              "date": "2015-05-24"
-            },
-            {
-              "origin": dest,
-              "destination": origin,
-              "date": "2015-08-16"
-            }
-          ]
-        }
+        ]
       }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=json.dumps(data), headers=headers) 
-    results[origin][dest] = response.json()
+    }
+  headers = {'Content-Type': 'application/json'}
+  response = requests.post(url, data=json.dumps(data), headers=headers) 
+  results[origin+'-'+dest] = response.json()
 
 """
 for i in xrange(len(airports)):
