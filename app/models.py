4cdaf10fc613017ecdf0464c71bf34704ddb9183
He Li <business.he.li@gmail.com>
2016-01-06 17:35:44 -0600

    finished models for trip and airport

--- app/models.py
+++ app/models.py
@@ -1,23 +1,35 @@
 from app import db
 
-trips = db.Table('trips',
-    db.Column('start_id', db.Integer, db.ForeignKey('airport.id')),
-    db.Column('end_id', db.Integer, db.ForeignKey('airport.id'))
-)
+class Trip(db.Model):
+    '''
+    Association Object
+    '''
+
+    __tablename__ = 'trip'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    airport_from = db.Column(db.Integer, db.ForeignKey('airport.id'))
+    airport_to = db.Column(db.Integer, db.ForeignKey('airport.id'))
+    price = db.Column(db.Float)
+    date = db.Column(db.Date)
 
 class Airport(db.Model):
+
+    __tablename__ = 'airport'
+
     id = db.Column(db.Integer, primary_key=True)
     iata = db.Column(db.String(8), index=True, unique=True)
     name = db.Column(db.String(120), index=True, unique=True)
-    location = db.Column(db.String(120))
+    city = db.Column(db.String(120))
+    region = db.Column(db.String(120))
+    country = db.Column(db.String(120))
 
-    destinations = db.relationship('Airport',
-                          secondary=trips,
-                          primaryjoin=(trips.c.start_id == id),
-                          secondaryjoin=(trips.c.end_id == id),
-                          backref=db.backref('starts', lazy='dynamic'),
-                          lazy='dynamic')
+    flying_from = db.relationship('Trip', backref='end', primaryjoin=(id==Trip.airport_to))
+    flying_to = db.relationship('Trip', backref='start', primaryjoin=(id==Trip.airport_from))
 
     def __repr__(self):
         return '<Airport: {0}; IATA: {1}>'.format(self.name, self.iata)
+
+
+
 
